de3027faf711caf8527b529e1405922c92e70dff
TobiasNorlund <tobias.norlund@tele2.se>
2014-03-06 16:38:22 +0100

    Börjat...

---
+++ Cleaner.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DustSucker2000
+{
+
+
+    class Cleaner
+    {
+        /// <summary>
+        /// Current position of the cleaner. Units in m
+        /// </summary>
+        public Position CurrentPosition { get; set; }
+
+        /// <summary>
+        /// Current pointing direction of the cleaner
+        /// </summary>
+        public Direction CurrentDirection { get; set; }
+
+
+        /// <summary>
+        /// Con
+        /// </summary>
+        private List<Actions> CurrentRoute { get; set; }
+
+        /// <summary>
+        /// Moves the cleaner according to the route and cleans the ground in the meanwhile
+        /// </summary>
+        public void Clean()
+        {
+            
+        }
+
+        /// <summary>
+        /// Loads a new route into the cleaner
+        /// </summary>
+        public void LoadRoute()
+        {
+
+        }
+
+    }
+
+    /// <summary>
+    /// Enumeration of possible directions of the cleaner
+    /// </summary>
+    enum Direction
+    {
+        North,
+        West,
+        South,
+        East
+    }
+
+    /// <summary>
+    /// Enumeration of the available actions
+    /// </summary>
+    enum Actions
+    {
+        MoveForward,
+        TurnRight,
+        TurnLeft
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace DustSucker2000
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();
         }
     }
 }
